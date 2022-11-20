f9c1fac588f3a55e76a18d50bdf136df7948d17f
leonard lebechi <leonell4fame@gmail.com>
2022-11-20 02:39:10 +0100

    favicon

--- adminheader.php
+++ adminheader.php
@@ -24,6 +24,10 @@ if (isset($_SESSION['log']) && $_SESSION['log'] == "#AdMin#") {
     <meta name="keywords" content="czar's Place,wristwatch,luxury watch,mechanical watch,
     time,timepiece,online store,watch store,watch collection,
     lagos,rolex,hublot,patek phillepe,">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon_io/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon_io/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon_io/favicon-16x16.png">
+<link rel="manifest" href="favicon_io/site.webmanifest">
 
     <link rel="stylesheet" href="bootstrap/bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="fontawesome/css/all.min.css">
--- indexheader.php
+++ indexheader.php
@@ -14,6 +14,10 @@ include_once "functions/admin.php";
   <meta name="keywords" content="czar's Place,wristwatch,luxury watch,mechanical watch,
     time,timepiece,online store,watch store,watch collection,
     lagos,rolex,hublot,patek phillepe,">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon_io/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon_io/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon_io/favicon-16x16.png">
+<link rel="manifest" href="favicon_io/site.webmanifest">
 
   <link rel="stylesheet" href="bootstrap/bootstrap/css/bootstrap.css">
   <link rel="stylesheet" href="fontawesome/css/all.min.css">
